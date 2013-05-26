03b75c8782e0330bd9832b2b5fcb295d330f1d93
Viktor Turskyi <koorchik@gmail.com>
2013-05-26 18:43:20 +0300

    First draft  implementation is ready

--- lib/LIVR/Contract.pm
+++ lib/LIVR/Contract.pm
@@ -1,124 +1,157 @@
 package LIVR::Contract;
 
 use strict;
+use warnings;
+
+use Exporter 'import';
+use Carp qw/croak/;
 
 use Validator::LIVR;
-use Class::Method::Modifier qw/install_modifier/;
+use Class::Method::Modifiers qw/install_modifier/;
+use LIVR::Contract::Exception;
 
-use Moo;
-
-has 'subname'           => ( is => 'rw', required => 1 );
-has 'package'           => ( is => 'rw', required => 1 );
-has 'ensures'           => ( is => 'rw' );
-has 'requires'          => ( is => 'rw' );
-has 'input_preparator'  => ( is => 'rw' );
-has 'output_preparator' => ( is => 'rw' );
-has 'on_fail'           => ( is => 'lazy' );
-
-has '_input_validator'   => (
-    is => 'lazy',
-    default => sub {
-        my $self = shift;
-        return Validator::LIVR->new($self->requires)->prepare();
-    }
-);
-
-has '_output_validator'  => (
-    is => 'lazy',
-    default => sub {
-        my $self = shift;
-        return Validator::LIVR->new($self->requires)->prepare();
-    }
-);
+our @EXPORT_OK = ( 'contract' );
+our @CARP_NOT = (__PACKAGE__);
 
 sub contract {
     my $subname = shift;
-    my $package = '';
+    croak "Subname is required" unless $subname;
 
     return __PACKAGE__->new(
         'subname' => $subname,
-        'package' => $package
+        'package' => ( caller )[ 0 ]
     );
+}
+
+sub new {
+    my ( $class, %args ) = @_;
+
+    croak '"subname" is required' unless $args{subname};
+    croak '"package" is required' unless $args{package};
+
+    my $self = bless {
+        subname           => $args{subname},
+        package           => $args{package},
+        ensures           => $args{requires},
+        requires          => $args{ensures},
+        input_preparator  => $args{input_preparator},
+        output_preparator => $args{output_preparator},
+        on_fail           => $args{on_fail},
+        input_validator   => undef,
+        output_validator  => undef,
+    }, $class;
+
+    return $self;
+}
+
+sub ensures {
+    my ( $self, $livr ) = @_;
+    $self->{ensures} = $livr;
+    return $self;
+}
+
+sub requires {
+    my ( $self, $livr ) = @_;
+    $self->{requires} = $livr;
+    return $self;
+}
+
+sub input_preparator {
+    my ( $self, $preparator ) = @_;
+    $self->{input_preparator} = $preparator;
+    return $self;
+}
+
+sub output_preparator {
+    my ( $self, $preparator ) = @_;
+    $self->{output_preparator} = $preparator;
+    return $self;
 }
 
 sub enable {
     my $self = shift;
 
-    install_modifier( $package, 'around', $self->subname, sub {
-        my $orig = shift;
-        my $input = $self->input_preparator->(@_);
-        $self->_validate_input($input);
+    install_modifier(
+        $self->{package},
+        'around',
+        $self->{subname},
+        sub {
+            my $orig  = shift;
 
-        my $output = wantarray ? [ $orig->(@_) ] : $orig->(@_);
+            my $input = $self->_get_input_preparator->( @_ );
+            $self->_validate_input( $input );
 
-        $self->_validate_output($output);
+            my $is_wantarray = wantarray;
+            my $output = $is_wantarray ? [ $orig->( @_ ) ] : $orig->( @_ );
+            
+            my $prepared_output = $self->_get_output_preparator->( $is_wantarray ? @$output : $output );
+            $self->_validate_output( $prepared_output );
 
-        return wantarray ? @$output : $output;
-    });
+            return $is_wantarray ? @$output : $output;
+        }
+    );
 }
 
 sub _validate_input {
-    my ($self, $input) = @_;
-    my $validator = $self->_input_validator;
+    my ( $self, $input ) = @_;
+    return unless $self->{requires};
 
-    if ( ! $validator->validate($input) ) {
-        $self->on_fail->( $self->package, $self->subname, $validator->get_errors() );
+    my $validator = $self->{input_validator} ||= Validator::LIVR->new( $self->{requires} )->prepare();
+
+    if ( !$validator->validate( $input ) ) {
+        $self->_get_on_fail->( 'input', $self->{package}, $self->{subname}, $validator->get_errors() );
     }
 }
 
 sub _validate_output {
-    my ($self, $output) = @_;
+    my ( $self, $output ) = @_;
+    return unless $self->{ensures};
 
-    my $validator = $self->_output_validator;
+    my $validator = $self->{output_validator} ||= Validator::LIVR->new( $self->{ensures} )->prepare();
 
-    if ( ! $validator->validate($input) ) {
-        $self->on_fail->( $self->package, $self->subname, $validator->get_errors() );
+    if ( !$validator->validate( $output ) ) {
+        $self->_get_on_fail->( 'output', $self->{package}, $self->{subname}, $validator->get_errors() );
     }
 }
 
-sub _build_on_fail {
-    return sub  {
-        my ($package, $subname, $errors) = @_;
-        die "$package: $subname";
-    }
+sub _get_input_preparator {
+    my $self = shift;
+
+    return $self->{input_preparator} ||= sub {
+        my ($self, %args) = @_;
+        return \%args;
+    };
 }
 
+sub _get_output_preparator {
+    my $self = shift;
 
-# sub new {
-#     my ($class, %args) = @_;
-#     my $self = {
-#         'subname'  => $args->{subname},
-#         'package'  => $args->{package},
-#         'ensures'  => {},
-#         'requires' => {},
-#         #...
-#     }
-# }
+    return $self->{output_preparator} ||=  sub {
+        my %res;
 
-# sub ensures {
-#     my ($self, $livr) = @_;
-#     $self->{ensures} = $livr;
-#     return $self;
-# }
+        for (my $i=0; $i< @_; $i++) {
+            $res{$i} = $_[$i];
+        }
 
-# sub requires {
-#     my ($self, $livr) = @_;
-#     $self->{requires} = $livr;
-#     return $self;
-# }
+        return \%res;
+    };
+}
 
-# sub input_preparator {
-#     my ($self, $preparator) = @_;
-#     $self->{input_preparator} = $preparator;
-#     return $self;
-# }
+sub _get_on_fail {
+    my $self = shift;
+    
+    return $self->{on_fail} ||=   sub {
+        my ( $type, $package, $subname, $errors ) = @_;
 
+        local $Carp::Internal{ (__PACKAGE__) } = 1;
 
-# sub output_preparator {
-#     my ($self, $preparator) = @_;
-#     $self->{output_preparator} = $preparator;
-#     return $self;
-# }
-
+        die LIVR::Contract::Exception->new(
+            type    => $type,
+            package => $package,
+            subname => $subname,
+            errors  => $errors
+        );
+    }
+}
 
 1;
--- t/01-base.t
+++ t/01-base.t
@@ -1,9 +1,66 @@
-#!perl -T
+#!/usr/bin/perl
 
-use Test::More tests => 1;
+use strict;
+use warnings;
+
+use FindBin;
+use lib "$FindBin::Bin/../lib";
+
+use Test::More;
 
 BEGIN {
-    use_ok( 'LIVR::Contract' ) || print "Bail out!\n";
+    use_ok( 'LIVR::Contract', 'contract') || print "Bail out!\n";
 }
 
-diag( "Testing LIVR::Contract $LIVR::Contract::VERSION, Perl $], $^X" );
+package MyTestingClass;
+
+use LIVR::Contract qw/contract/;
+
+contract('my_method')->requires({
+    name      => [ 'required' ],
+    id        => [ 'required', 'positive_integer' ]
+})->ensures({
+    0 => ['required', 'positive_integer' ]
+})->enable();
+
+
+sub my_method {
+    return 0;
+}
+
+package main;
+
+subtest 'Test "requires" rules' => sub {
+    eval {
+        MyTestingClass->my_method(id => '0');
+    };
+
+    ok($@, 'Should throw exception');
+    isa_ok($@, 'LIVR::Contract::Exception');
+    
+    is($@->type, 'input', 'Should be "input" type');
+    is($@->subname, 'my_method', 'Should contain name of failed method');
+    is($@->package, 'MyTestingClass', 'Should contain name of failed package');
+
+    is_deeply($@->errors, {name => 'REQUIRED', id => 'NOT_POSITIVE_INTEGER'}, 'Should  contain errors in Validator::LIVR format');
+};
+
+
+subtest 'Test "ensures" rules' => sub {
+    eval {
+        MyTestingClass->my_method(name => 'Viktor', id => 100 );
+    };
+
+    ok($@, 'Should throw exception');
+    isa_ok($@, 'LIVR::Contract::Exception');
+    
+    is($@->type, 'output', 'Should be "input" type');
+    is($@->subname, 'my_method', 'Should contain name of failed method');
+    is($@->package, 'MyTestingClass', 'Should contain name of failed package');
+
+    is_deeply($@->errors, {0 => 'NOT_POSITIVE_INTEGER'}, 'Should  contain errors in Validator::LIVR format');
+};
+
+done_testing();
+
+1;
