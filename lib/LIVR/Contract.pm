cdad85da9c66e0caadc36cb712879690a76416fd
Viktor Turskyi <koorchik@gmail.com>
2013-06-09 00:35:34 +0300

    Updated version

--- lib/LIVR/Contract.pm
+++ lib/LIVR/Contract.pm
@@ -14,7 +14,7 @@ use Scalar::Util qw/blessed/;
 our @EXPORT = ( 'contract' );
 our @CARP_NOT = (__PACKAGE__);
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 sub contract {
     my ( $subname, %args ) = @_;
